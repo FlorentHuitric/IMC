f5c934f7126193c306209ab4e700ffba2cb540d8
FuroRen <florent.huitric@gmail.com>
2022-03-26 16:19:21 +0100

    valid

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 function calcIMC(Poids,Taille){
-    var imc= Poids/(Math.pow(Taille/100,2));
+    const imc= Poids/(Math.pow(Taille/100,2));
     return imc;
 }
+const button = document.getElementById(button);
+const Taille = document.getElementById(Taille);
+const Poids = document.getElementById(Poids);
+const result = document.getElementById(result);
 
 
-
-document.querySelector('button').addEventListener("click",function(){
-    querySelector("h3").imc;
+document.getElementById(button).addEventListener("click",function(Poids,Taille){
+    getElementById(result).calcIMC;
 });
 
 
