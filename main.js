3bd6d6df4fa59bb11bd7fdb9bc5dc600e6c5dc40
FuroRen <florent.huitric@gmail.com>
2022-03-27 21:45:01 +0200

    valid

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const button = document.getElementById("button");
 const taille = document.getElementById("taille");
 const poids = document.getElementById("poids");
 const result = document.getElementById("result");
+const cheerUp = document.getElementById("cheerUp");
+const URL = "https://www.ameli.fr/finistere/assure/sante/themes/surpoids-obesite-adulte/definition-causes-risques";
+const URL2 = "https://solidarites-sante.gouv.fr/prevention-en-sante/preserver-sa-sante/nutrition/article/prevention-des-troubles-du-comportement-alimentaire-chez-les-jeunes"
 
 const calcIMC = (poids, taille) => (poids/(Math.pow(taille/100,2))).toFixed(1);
 
@@ -18,18 +21,16 @@ button.addEventListener("click",function(){
 */   
     switch(true) {
         case imc < 18.5:
-            console.log("Tu es en sous poids !");
+            cheerUp.innerHTML="Tu es en sous poids !".link(URL2);
             result.style.backgroundColor="#e55039";
             break;
         case imc>24.9:
-            console.log("Tu es en surpoids !")
+            cheerUp.innerHTML="Tu es en surpoids !".link(URL);
             result.style.backgroundColor="#e55039";
             break;
         case (imc>=18.5 && imc<=24.9):
-            console.log("Tu tiens la forme, super !")
+            cheerUp.innerText=("Tu tiens la forme, super !")
             result.style.backgroundColor="#78e08f";
             break;    
     }
 });
-
-
