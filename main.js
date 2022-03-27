719bf241b4263e3f2a164e63842a1ba0c1340fde
FuroRen <florent.huitric@gmail.com>
2022-03-27 16:19:00 +0200

    modif

--- main.js
+++ main.js
@@ -2,23 +2,34 @@ const button = document.getElementById("button");
 const taille = document.getElementById("taille");
 const poids = document.getElementById("poids");
 const result = document.getElementById("result");
-const result1 = "votre IMC est ";
 
-function calcIMC(Poids,Taille){
-    return imc=(Poids/(Math.pow(Taille/100,2))).toFixed(1);
-}
-
-
+const calcIMC = (poids, taille) => (poids/(Math.pow(taille/100,2))).toFixed(1);
 
 button.addEventListener("click",function(){
+
     const imc = calcIMC(poids.value, taille.value);
-    result.innerHTML=result1 + imc;
-    console.log(imc);
+    result.innerHTML=`Votre IMC est de `+(imc);
+/*   utilisation du switch/case préférable
     if (imc<=18.5 || imc>=24.9){
         result.style.backgroundColor="#e55039";
     } else{
         result.style.backgroundColor="#78e08f";
     };
+*/   
+    switch(true) {
+        case imc < 18.5:
+            console.log("Tu es en sous poids !");
+            result.style.backgroundColor="#e55039";
+            break;
+        case imc>24.9:
+            console.log("Tu es en surpoids !")
+            result.style.backgroundColor="#e55039";
+            break;
+        case (imc>=18.5 && imc<=24.9):
+            console.log("Tu tiens la forme, super !")
+            result.style.backgroundColor="#78e08f";
+            break;    
+    }
 });
 
 
