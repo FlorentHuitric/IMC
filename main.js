5357225e6388d0ca869b6fdfb0415a62bdfb11b6
FuroRen <florent.huitric@gmail.com>
2022-03-27 12:03:43 +0200

    modif

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const button = document.getElementById("button");
 const taille = document.getElementById("taille");
 const poids = document.getElementById("poids");
 const result = document.getElementById("result");
-
+const result1 = "votre IMC est ";
 
 function calcIMC(Poids,Taille){
     return imc=(Poids/(Math.pow(Taille/100,2))).toFixed(1);
@@ -12,7 +12,13 @@ function calcIMC(Poids,Taille){
 
 button.addEventListener("click",function(){
     const imc = calcIMC(poids.value, taille.value);
+    result.innerHTML=result1 + imc;
     console.log(imc);
+    if (imc<=18.5 || imc>=24.9){
+        result.style.backgroundColor="#e55039";
+    } else{
+        result.style.backgroundColor="#78e08f";
+    };
 });
 
 
